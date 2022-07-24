becea86ff2a0fdad33528d33370fcc6ccaff5583
Aaron Elias <aaronelias007@gmail.com>
2022-07-23 21:10:17 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ underway = Restaurant.create(name: "Underway", slogan: "Eat Subs", role: 2)
 papajames = Restaurant.create(name: "Papa James", slogan: "Eat Za", role: 3)
 
 
+Meal.create(name: "Cake", calories: 9000, main_ingredient: "sugar", country_of_origin: "America", restaurant: "Tarducks")
+Meal.create(name: "Scone", calories: 800, main_ingredient: "sugar", country_of_origin: "America", restaurant: "Tarducks")
 
 Meal.create(name: "Beef Sub", calories: 900, main_ingredient: "beef", country_of_origin: "America", restaruant: "Underway")
 Meal.create(name: "Turkey Sub", calories: 300, main_ingredient: "turkey", country_of_origin: "America", restaurant: "Underway")
