9073a10fed377308c5ad52f22a7e2bf5bbd5c94d
Aaron Elias <aaronelias007@gmail.com>
2022-07-23 21:08:53 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
 puts "🌱 Seeding spices..."
 
-tarducks = Restaurant.create(name: "Tarducks", slogan: "Drink Mud", role: 1)
-underway = Restaurant.create(name: "Underway", slogan: "Eat Subs", role: 2)
-papajames = Restaurant.create(name: "Papa James", slogan: "Eat Za", role: 3)
 
 Meal.create(name: "Cake", calories: 9000, main_ingredient: "sugar", country_of_origin: "America", restaurant: "Tarducks")
 Meal.create(name: "Scone", calories: 800, main_ingredient: "sugar", country_of_origin: "America", restaurant: "Tarducks")
